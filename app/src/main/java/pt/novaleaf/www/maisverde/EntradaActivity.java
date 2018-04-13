772c1130c69f0dab6039460e53eabd6d7616aa23
Hugo Mochao <hugomochao@gmail.com>
2018-04-13 20:19:55 +0100

    dred

---
+++ app/src/main/java/pt/novaleaf/www/maisverde/EntradaActivity.java
@@ -0,0 +1,49 @@
+package pt.novaleaf.www.maisverde;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class EntradaActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada);
+
+        imageView = findViewById(R.id.mImage);
+
+        RotateAnimation rotate = new RotateAnimation(0, 180*20, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(5000*20);
+        rotate.setInterpolator(new LinearInterpolator());
+
+        imageView.startAnimation(rotate);
+
+
+        passActivity();
+
+    }
+
+
+    public void passActivity(){
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(EntradaActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        }, 4000L);
+    }
+}
---
+++ app/src/main/java/pt/novaleaf/www/maisverde/LoginActivity.java
@@ -0,0 +1,29 @@
+package pt.novaleaf.www.maisverde;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainMenuActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
